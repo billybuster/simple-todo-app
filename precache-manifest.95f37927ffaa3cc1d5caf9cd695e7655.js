67d9bd0a53e3790e16cb7cc46324c603ea46b935
billybuster <postlethwaiteandy@gmail.com>
2020-08-14 10:51:03 +0100

    Updates

---
+++ precache-manifest.95f37927ffaa3cc1d5caf9cd695e7655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11c07758638da91372b5d1428ece700",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "b843c0a2a23074bb9af3",
+    "url": "/simple-todo-app/static/css/main.645525b1.chunk.css"
+  },
+  {
+    "revision": "fba20ae7c58d4641a545",
+    "url": "/simple-todo-app/static/js/2.2d76edf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.2d76edf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b843c0a2a23074bb9af3",
+    "url": "/simple-todo-app/static/js/main.06560c67.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
